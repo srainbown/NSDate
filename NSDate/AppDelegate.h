71ff5c25babe895b4cd326b9117d3dd53f4a97e3
紫川秀 <tszy111@126.com>
2017-05-03 14:15:38 +0800

    Initial Commit

---
+++ NSDate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDate
+//
+//  Created by 李自杨 on 2017/5/3.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSDate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NSDate
+//
+//  Created by 李自杨 on 2017/5/3.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
